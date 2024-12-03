72d097a0672527c6218f9c7894171b0abe4287e8
Pascal Steininger <stp34852@ads.uni-regensburg.de>
2024-12-03 13:39:29 +0100

    Continiued Work on Installer

--- installer/installer.py
+++ installer/installer.py
@@ -1,13 +1,15 @@
+import win32com.client
 from requests import get
 from zipfile import ZipFile
-from os import environ, path, remove
+from os import environ, path, remove, rename
 from sys import exit
 from subprocess import Popen
+
 
 def update() -> None:
 
     APP_DATA_PATH:str = path.join(environ['APPDATA'], 'Time Project PYQT')
-    FINISHED_PATH:str = path.join(environ['APPDATA'], 'Time Project PYQT', "Distribution", "Time_Tracker")
+    FINISHED_PATH:str = path.join(environ['APPDATA'], 'Time Project PYQT', "Distribution", "Time_Tracker.dist")
 
     master_version =  get("https://raw.githubusercontent.com/Pasquotcho/TimeTracker/refs/heads/master/version").text.strip()
 
@@ -24,14 +26,23 @@ def update() -> None:
             print("Extracted!")
 
     remove("dist.zip")
-
+    if path.exists(f"{FINISHED_PATH}/Time Tracker.exe"):
+        remove(f"{FINISHED_PATH}/Time Tracker.exe")
+    rename(f"{FINISHED_PATH}/main.exe", f"{FINISHED_PATH}/Time Tracker.exe")
     # opens app in background
     Popen([f"{FINISHED_PATH}/Time Tracker.exe"])
 
-    # sys exit
-    exit()
+    # Shortcut
+    desktop_path = path.join(environ["USERPROFILE"], "Desktop")
+    shell = win32com.client.Dispatch("WScript.Shell")
 
-    print("Updated!")
+    shortcut = shell.CreateShortcut(f"{desktop_path}/Time Tracker.lnk")
+    shortcut.TargetPath = f"{FINISHED_PATH}/Time Tracker.exe"
+    shortcut.save()
+
+    print("Installed")
+
+
 
 if __name__=="__main__":
     update()
