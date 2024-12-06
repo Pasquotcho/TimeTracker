50247b092fe495ade2594c35f91925ee3be44df0
Pascal <steininger.pascal@outlook.de>
2024-12-06 16:17:18 +0100

    Download Update 3

--- updater.py
+++ updater.py
@@ -1,27 +1,27 @@
-# from requests import get
-# import sys
-# from os import chdir, path, getcwd
-# from subprocess import Popen
-# from version import version
+from requests import get
+import sys
+from os import chdir, path, getcwd
+from subprocess import Popen
+from version import version
 
-# # Current working directory
-# CWD = getcwd()
+# Current working directory
+CWD = getcwd()
 
-# def check_update() -> int:
+def check_update() -> int:
 
-#     # Get latest Version
-#     request:str =  get("https://api.github.com/repos/Pasquotcho/TimeTracker/releases/latest")
-#     data:dict = request.json()
-#     master_version:str = data["tag_name"]
+    # Get latest Version
+    request:str =  get("https://api.github.com/repos/Pasquotcho/TimeTracker/releases/latest")
+    data:dict = request.json()
+    master_version:str = data["tag_name"]
 
-#     if version == master_version:
-#         print("Version up to date!")
-#         return 0
+    if version == master_version:
+        print("Version up to date!")
+        return 0
     
-#     else:
-#         print("Version mismatch!")
-#         return 1
+    else:
+        print("Version mismatch!")
+        return 1
 
-# def start_update():
-#     Popen("../../installer.exe")
-#     sys.exit()
+def start_update():
+    Popen("../../installer.exe")
+    sys.exit()
